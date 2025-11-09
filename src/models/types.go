a085b6d89a61b1311eb29b931bde2c94d484f487
Carlosss2 <147357485+Carlosss2@users.noreply.github.com>
2025-11-08 23:35:11 -0600

    delte bug

--- src/models/types.go
+++ src/models/types.go
@@ -1,17 +1,1 @@
 package models
-
-import "time"
-
-
-type StatusUpdate struct {
-	Timestamp     time.Time
-	IsConnected   bool    
-	IsRunning     bool    
-	MessagesSent  int64   
-	Error         string  
-}
-
-const (
-	CmdStart = "START" 
-	CmdStop  = "STOP"  
-)
